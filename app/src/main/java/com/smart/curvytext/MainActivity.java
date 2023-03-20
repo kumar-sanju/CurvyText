3973ef38b6aa005220dd0b14035eb12779fa4cc8
SanjayAndroid <sanjay.mahapatra@muvi.com>
2023-03-20 22:48:34 +0530

    rotate my curvy text

--- app/src/main/java/com/smart/curvytext/MainActivity.java
+++ app/src/main/java/com/smart/curvytext/MainActivity.java
@@ -3,9 +3,17 @@ package com.smart.curvytext;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,11 +28,25 @@ public class MainActivity extends AppCompatActivity {
         textArc.setRadius(255);
         textArc.setCenterAngle(-90);
         textArc.setTextColor(R.color.teal_200);
-        textArc.setTextSize(72);
+        textArc.setTextSize(60);
         textArc.setFontFamily(ResourcesCompat.getFont(this, R.font.krabuler));
 
         textArc.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
         ((FrameLayout)findViewById(R.id.flTextArc)).addView(textArc);
+
+        CurvyText textArc2 = new CurvyText(this);
+        textArc2.setText("Second Curvy text was added");
+        textArc2.setRadius(110);
+        textArc2.setCenterAngle(-90);
+        textArc2.setTextColor(R.color.teal_200);
+        textArc2.setTextSize(35);
+        textArc2.setFontFamily(ResourcesCompat.getFont(this, R.font.krabuler));
+
+        textArc2.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        ((FrameLayout)findViewById(R.id.flTextArc2)).addView(textArc2);
+
+        Animation animationRotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        ((FrameLayout)findViewById(R.id.flTextArc2)).startAnimation(animationRotate);
 
     }
 }
